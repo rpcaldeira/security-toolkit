fdb105ce6f7a8e93405a2c0aa1f5ea26ad183844
Rui Pedro Caldeira <TheCakeIsALie@rpcnet.net>
2020-11-09 22:24:16 +0000

    Added SPFCheckerTest

--- mail/src/test/java/net/rpcnet/securitytoolkit/mail/spf/SPFCheckerTest.java
+++ mail/src/test/java/net/rpcnet/securitytoolkit/mail/spf/SPFCheckerTest.java
@@ -2,12 +2,27 @@ package net.rpcnet.securitytoolkit.mail.spf;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SPFCheckerTest {
 
+    public static final String SPF_GOOGLE_COM = "_spf.google.com";
+    public static final String RPCNET_NET = "rpcnet.net";
+
     @Test
     void getSPF() {
-        System.out.println(SPFChecker.getSPF("rpcnet.net"));
+        Optional<SPFResult> spf = SPFChecker.getSPF(RPCNET_NET);
+        assertTrue(spf.isPresent());
+        SPFResult spfResult = spf.get();
+
+        assertEquals(Optional.of(SPFQualifier.FAIL), spfResult.getAll());
+        assertEquals(Collections.emptyList(), spfResult.getARecord());
+        assertEquals(Collections.singletonList(ImmutableSPFElement.builder().qualifier(SPFQualifier.PASS).value(SPF_GOOGLE_COM).build()), spfResult.getInclude());
+        assertEquals(Optional.of(SPFQualifier.PASS), spfResult.getMailExchange());
+        assertEquals(Optional.of(1), spfResult.getVersion());
     }
 }
